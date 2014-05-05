5e8012cb1900802ab95d99778afd89e7a6eeab75
Enrique Rivas Rivera <savir81@hotmail.com>
2014-05-05 13:41:10 -0700

    added settings for deploying

--- config.rb
+++ config.rb
@@ -13,12 +13,12 @@ activate :automatic_image_sizes
 activate :syntax # code highlighting
 
 # github userpages deploy
-#activate :deploy do |deploy|
-  #deploy.method = :git
-  #deploy.build_before = true
-  #deploy.branch   = "master"
-  #deploy.remote   = "git@github.com:username/username.github.io.git"
-#end
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+  deploy.branch   = "master"
+  deploy.remote   = "git@github.com:mrromita/mrromita.github.io.git"
+end
 
 # github project pages deploy
 #activate :deploy do |deploy|
